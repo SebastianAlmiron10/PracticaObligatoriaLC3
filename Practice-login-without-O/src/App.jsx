5458a5508881f83ec78a15d306587d0ef7a5747c
Sebastian Almiron <sebastianr_almiron@hotmail.com>
2024-05-04 00:13:41 -0300

    Practice 3 complete

---
+++ Practice-login-without-O/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Login from './components/login/Login'
+
+function App() {
+
+  return (
+    <div className='w-full h-[100vh] bg-[#333] flex justify-center items-center'>
+      <Login/>
+    </div>
+  )
+}
+
+export default App
---
+++ Practice-login-without-O/src/components/login/Login.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+
+const Login = () => {
+
+    const [Username, setUsername] = useState('')
+
+    const handleChangeUsername = (e) => {
+        const value = e.target.value
+        if(value.toLowerCase().includes('o')){
+            alert('Por favor, ¡Nombres de usuario sin la letra o!')
+        }else{
+            setUsername(value)
+        }
+    }
+
+    const onClickRegister = () => {
+        if(Username.toLowerCase().includes('o')){
+            alert('Usuario inválido para registrarse')
+        }else{
+            alert('¡Usuario registrado correctamente!')
+        }
+    }
+
+    return (
+            <form action="" className='flex flex-col w-96 gap-4'>
+                <label className='text-xl font-semibold text-white'>Username:</label>
+                <input 
+                    type="text" 
+                    name="" 
+                    id="" 
+                    className='rounded-lg p-2' 
+                    placeholder='username...'
+                    value={Username}
+                    onChange={handleChangeUsername}
+                    />
+                <div className='w-full flex justify-center'>
+                    <button type="submit" onClick={onClickRegister} className='bg-green-400 rounded-lg text-white p-2 w-44'>Registrarse</button>
+                </div>
+                <p className='text-white'>{Username}</p>
+            </form>
+    );
+};
+
+export default Login;
